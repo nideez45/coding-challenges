bd1c97081e02e5b6881dd56b462ad28932946c2b
NideeshN <112001028@smail.iitpkd.ac.in>
2023-12-24 20:47:44 +0530

    basic statistics

--- 41-http-load-tester/load-tester.py
+++ 41-http-load-tester/load-tester.py
@@ -1,10 +1,51 @@
 import httpx
 import asyncio
 import argparse
+import time
 
+async def perform_load_test(url,num_requests,concurrent_requests,http_method):
+    total_rounds = int(num_requests/concurrent_requests)
+    
+    # the ones with status code 200,300
+    success = 0
+    # the ones with status code 400,500
+    failure = 0
+    
+    ttfb = []
+    total_times = []
+    
+    for _ in range(total_rounds):
+        async with httpx.AsyncClient() as client:
+            tasks = []
+            for _ in range(concurrent_requests):
+                start_time = time.time()
+                task = client.get(url)
+                tasks.append((start_time, task))
 
+            results = await asyncio.gather(*[task[1] for task in tasks])
 
-def main():
+            for start_time, result in zip(tasks, results):
+                status = int(result.status_code / 100)
+                end_time = time.time()
+                total_time = end_time - start_time[0]
+                ttfb_time = result.elapsed.total_seconds()
+
+                ttfb.append(ttfb_time)
+                total_times.append(total_time)
+
+                if status == 2 or status == 3:
+                    success += 1
+                else:
+                    failure += 1
+
+    print("Results....")
+    print("Successful requests (2xx, 3xx):", success)
+    print("Failed requests (4xx, 5xx):", failure)
+    print("Time to First Byte (TTFB):", ttfb)
+    print("Total Time:", total_times)
+    
+    
+async def main():
     parser = argparse.ArgumentParser(description='Http load tester')
     
     # Required arguments
@@ -30,8 +71,8 @@ def main():
         exit()
     
     print(url,num_requests,concurrent_requests,http_method)
-    #perform_load_test(url,num_requests,concurrent_requests,http_method)
+    await perform_load_test(url,num_requests,concurrent_requests,http_method)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
     
