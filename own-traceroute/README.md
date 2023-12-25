4dc74fdc795833daefc664f7befaabbcf15af0cf
NideeshN <112001028@smail.iitpkd.ac.in>
2023-12-25 20:18:17 +0530

    changed logic

--- own-traceroute/README.md
+++ own-traceroute/README.md
@@ -13,24 +13,21 @@ options:
 ```
 Example
 ```
-% sudo python3 ctraceroute.py google.com
-Traceroute to google.com (142.250.193.110)... max 30 hops
-1 _gateway ( 192.168.43.1 ) 47.92 ms
-2 * * *
+% sudo sudo python3 ctraceroute.py twitter.com
+Traceroute to twitter.com (104.244.42.193)... max 30 hops
+1 _gateway (192.168.43.1) 18.11 ms
+2 192.168.29.10 (192.168.29.10) 117.96 ms
 3 * * *
-4 * * *
+4 192.168.31.20 (192.168.31.20) 57.9 ms
 5 * * *
 6 * * *
-7 * * *
-8 nsg-static-113.206.71.182.airtel.in ( 182.71.206.113 ) 81.15 ms
+7 nsg-static-117.206.71.182.airtel.in (182.71.206.117) 49.6 ms
+8 * * *
 9 * * *
 10 * * *
 11 * * *
 12 * * *
-13 * * *
+13 ae1.3115.edge7.London1.level3.net (4.69.166.2) 4700.67 ms
 14 * * *
-15 * * *
-16 * * *
-17 * * *
-18 maa05s24-in-f14.1e100.net ( 142.250.193.110 ) 83.47 ms
+15 104.244.42.193 (104.244.42.193) 3888.68 ms
 ```
--- own-traceroute/ctraceroute.py
+++ own-traceroute/ctraceroute.py
@@ -2,9 +2,9 @@ import socket
 import argparse
 import threading
 import time
-done = False
 
 Time = {}
+ans = {}
 
 def reverse_dns_lookup(ip_address):
     try:
@@ -20,44 +20,47 @@ def receive_icmp(domain,ipv4_address):
     
     icmp_socket = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
     icmp_socket.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
-    
-    expected_port = 33434
-    hop = 1
+    icmp_socket.settimeout(3)
     while True:
-        rec_packet, addr = icmp_socket.recvfrom(4096)
+        try:
+            rec_packet, addr = icmp_socket.recvfrom(4096)
+        except socket.timeout:
+            break
         addr = addr[0]
         src_port = int.from_bytes(rec_packet[-6:-4], byteorder='big')
         if src_port<33434 or src_port>33464:
             continue
-        
-        if src_port == expected_port:
-            print(hop,reverse_dns_lookup(addr),"(",addr,")",round((time.time()-Time[src_port])*1000,2),"ms")
-            expected_port+=1
-            hop+=1
-        elif src_port>expected_port:
-            diff = src_port-expected_port+1
-            for _ in range(diff):
-                print(hop+_,"* * *")
-            hop+=diff
-            print(hop,reverse_dns_lookup(addr),"(",addr,")",round((time.time()-Time[src_port])*1000,2),"ms")
-            expected_port=src_port+1
-            hop+=1
-        if addr == ipv4_address:
-            done = True
-            break
+        ans[src_port] = f"{reverse_dns_lookup(addr)} ({addr}) {round((time.time() - Time[src_port]) * 1000, 2)} ms"
+    
+ 
 def send_udp(domain):
     dst_port = 33434
     ttl = 1
     for i in range(30):
-        if done:
-            break
-        
         client_socket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
         client_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
         client_socket.sendto("".encode(),(domain,dst_port))
         Time[dst_port] = time.time()
         dst_port+=1
         ttl+=1
+    
+def print_result(ipv4_address):
+    lst = []
+    for key,value in ans.items():
+        lst.append((key-33434+1,value))
+    lst = sorted(lst)
+    hop = 1
+    for a,b in lst:
+        if a == hop:
+            print(hop,b)
+        else:
+            while hop<a:
+                print(hop,"* * *")
+                hop+=1
+            print(hop,b)
+        hop+=1
+        if ipv4_address in b:
+            break
     
 def main():
     parser = argparse.ArgumentParser(description='Custom traceroute')
@@ -70,7 +73,12 @@ def main():
     icmp_thread = threading.Thread(target=receive_icmp,args=(domain,ipv4_address,))
     udp_thread.start()
     icmp_thread.start()
-    
+    time.sleep(5)
+    result_thread = threading.Thread(target=print_result,args=(ipv4_address,))
+    result_thread.start()
+    result_thread.join()
+    udp_thread.join()
+    icmp_thread.join()
     
 if __name__ == "__main__":
     main()
