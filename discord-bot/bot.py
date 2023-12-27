4151e9c7ed29e31c4f340ff3ee2b7a50247d7be6
NideeshN <112001028@smail.iitpkd.ac.in>
2023-12-27 21:52:20 +0530

    added quote command

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -1,8 +1,9 @@
 import os
-
+import requests
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import random 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,7 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Define intents
 intents = discord.Intents.all()
 
-client = discord.Client(intents=intents,command_prefix = "!")
+client = commands.Bot(intents=intents,command_prefix = "!")
 
 @client.event
 async def on_ready():
@@ -21,5 +22,15 @@ async def on_message(message):
     content = message.content
     if content == 'Hello':
         await message.channel.send('Hello, {}'.format(message.author))
+    await client.process_commands(message) 
+
+@client.command(name='quote', help='Gives a random quote!')
+async def get_quote(ctx):
+    quoteid = random.randint(1,100)
+    response = requests.get("https://dummyjson.com/quote/{}".format(quoteid))
+    quote = response.json()['quote']
+    await ctx.send(quote)
+
+
 
 client.run(TOKEN)
