c5bf79adb9c1dc204740f19d821029e0a95cc4e3
NideeshN <112001028@smail.iitpkd.ac.in>
2023-12-28 15:19:12 +0530

    token-bucket algo

--- rate-limiter/server.py
+++ rate-limiter/server.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI
 from starlette.middleware.base import BaseHTTPMiddleware
 from token_bucket import token_bucket_middleware
 
+class TokenBucketMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        if request.url.path == "/limited":
+            my_middleware = token_bucket_middleware(1, 1)
+
+            return await my_middleware(request, call_next)
+        else:
+            return await call_next(request)
 
 
 app = FastAPI()
 
-my_middleware = token_bucket_middleware()
-app.add_middleware(BaseHTTPMiddleware,dispatch=my_middleware)
+app.add_middleware(TokenBucketMiddleware)
 
 @app.get("/unlimited")
 async def unlimited():
-    return {"message": "Unlimited! Let's Go!"}
+    return "Unlimited! Let's Go!"
 
 @app.get("/limited")
 async def limited():
-    return {"message": "Limited, don't overuse me!"}
+    return "Limited, don't overuse me!"
--- rate-limiter/token_bucket.py
+++ rate-limiter/token_bucket.py
@@ -1,8 +1,33 @@
 from fastapi import Request
+import time
+from fastapi.responses import JSONResponse
+
 
 class token_bucket_middleware:
-    async def __call__(self, request: Request, call_next):
-        print("Middleware A - Request Phase")
-        response = await call_next(request)
-        print("Middleware A - Response Phase")
-        return response
+    bucket = {}
+    def __init__(self,capacity:int,tps:float) :
+        self.capacity = capacity
+        self.tps = tps
+       
+    
+    async def refill_bucket(self,client_ip):
+        cur_time = time.time()
+        diff = cur_time-self.bucket[client_ip][1]
+        to_add = ((diff//1)*self.tps)//1
+        self.bucket[client_ip][0] = min(self.bucket[client_ip][0]+to_add,self.capacity)
+    
+    async def __call__ (self, request: Request, call_next):
+        client_ip = request.client.host
+        if client_ip not in self.bucket.keys():
+            self.bucket[client_ip] = [self.capacity,time.time()]
+            
+        await self.refill_bucket(client_ip)
+        if self.bucket[client_ip][0]>0:
+            self.bucket[client_ip][0] -= 1
+            self.bucket[client_ip][1] = time.time()
+            return await call_next(request)
+        
+        content = "Rate limit exceeded. Too many requests"
+        return JSONResponse(content=content, status_code=429)
+
+    
